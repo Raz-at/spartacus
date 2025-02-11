67dbd609eb08349c7647b5564439f65c4db43254
Raz-at <Raz-at@users.noreply.github.com>
2025-02-11 07:48:04 +0000

    deploy: ee55809807bf1cb4e595c2093ca33e5206603ea3

--- js/deployment.js
+++ js/deployment.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const deploymentBtn = document.getElementById('deploymentBtn');
 
-    // Add event listener to the "Deployment" button
     deploymentBtn.addEventListener('click', () => {
         console.log("Deployment script loaded");
         document.getElementById('message').innerText = 'This is deployment';
--- js/production.js
+++ js/production.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const productionBtn = document.getElementById('productionBtn');
 
-    // Add event listener to the "Production" button
     productionBtn.addEventListener('click', () => {
         console.log("Production script loaded");
         document.getElementById('message').innerText = 'This is production';
